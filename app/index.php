133ea1fc37b0a6440362c48ef44e9edf81814a32
AmmoniteNetworks <chris@ammonitenetworks.com>
2013-05-13 11:38:54 -0600

    adding the logic of the winners and losers

---
+++ app/index.php
@@ -0,0 +1,85 @@
+<?php
+
+$f3=require('lib/base.php');
+
+$f3->config('config.ini');
+
+$f3->route('GET|POST /',
+	function($f3) {
+
+	$maxplaysbeforewin = 200;	
+	$db=new DB\Jig('db/data/',DB\Jig::FORMAT_JSON);
+	$game=new DB\Jig\Mapper($db,'game');
+	$game->load(array('@game=?','1'));
+	$game->plays++;
+
+	if($game->won == 0) {
+		if($game->plays > 1) {
+
+			$chance = rand($game->plays, $maxplaysbeforewin); 
+			$win = rand($game->plays, $maxplaysbeforewin); 
+
+			echo 'chance:' . $chance;
+			echo 'win:' . $win;	
+
+			if($chance === $win) {
+
+				$game->winner = 1;
+				$game->won = 1;
+			}
+		} 
+	} else {
+		$game->winner = 0;
+	}
+	
+	$game->save();
+
+	$response = array('gameon'=>$game->gameon, 'plays'=>$game->plays, 'winner'=> $game->winner, 'won'=> $game->won );
+	
+	echo json_encode($response);
+
+	}
+);
+
+$f3->route('GET /luanch/@key',
+	function($f3) {
+		$key = $f3->get('PARAMS.key');
+		if($key ==  'A3FS7J') {
+			$db=new DB\Jig('db/data/',DB\Jig::FORMAT_JSON);
+			$game=new DB\Jig\Mapper($db,'game');
+			$game->load(array('@game=?','1'));
+			$game->gameon=1;
+			$game->plays=0;
+			$game->winner=0;
+			$game->won=0;
+			$game->save();
+
+			echo 'Game is ready';
+		} else {
+			echo 'invalid key';
+		}
+		
+
+	}
+);
+$f3->route('GET /gameoff/@key',
+	function($f3) {
+		$key = $f3->get('PARAMS.key');
+		if($key ==  'A3FS7J') {
+			$db=new DB\Jig('db/data/',DB\Jig::FORMAT_JSON);
+			$game=new DB\Jig\Mapper($db,'game');
+			$game->load(array('@game=?','1'));
+			$game->gameon=0;
+			$game->plays=0;
+			$game->winner=0;
+			$game->won=0;
+			$game->save();
+	
+			echo 'Game is off';
+		} else {
+			echo 'invalid key';
+		}
+	}
+);
+
+$f3->run();
